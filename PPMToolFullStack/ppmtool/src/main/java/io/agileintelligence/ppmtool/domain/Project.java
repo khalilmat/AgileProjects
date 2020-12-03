9a25318482c07b7d7e88ea7391d5b3b33b2704da
kmat1912 <kmry>
2020-12-02 20:18:08 -0500

    Project Object & Project Repository

--- PPMToolFullStack/ppmtool/src/main/java/io/agileintelligence/ppmtool/domain/Project.java
+++ PPMToolFullStack/ppmtool/src/main/java/io/agileintelligence/ppmtool/domain/Project.java
@@ -11,7 +11,7 @@ import javax.persistence.PreUpdate;
 
 @Entity
 public class Project {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType .IDENTITY)
     private Long id;
@@ -24,6 +24,74 @@ public class Project {
     private Date created_At;
     private Date updated_At;
 
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProjectName() {
+        return this.projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getProjectIdentifier() {
+        return this.projectIdentifier;
+    }
+
+    public void setProjectIdentifier(String projectIdentifier) {
+        this.projectIdentifier = projectIdentifier;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStart_date() {
+        return this.start_date;
+    }
+
+    public void setStart_date(Date start_date) {
+        this.start_date = start_date;
+    }
+
+    public Date getEnd_date() {
+        return this.end_date;
+    }
+
+    public void setEnd_date(Date end_date) {
+        this.end_date = end_date;
+    }
+
+    public Date getCreated_At() {
+        return this.created_At;
+    }
+
+    public void setCreated_At(Date created_At) {
+        this.created_At = created_At;
+    }
+
+    public Date getUpdated_At() {
+        return this.updated_At;
+    }
+
+    public void setUpdated_At(Date updated_At) {
+        this.updated_At = updated_At;
+    }
+
+    //No argument constructor 
+    public Project() {
+    }
+
     @PrePersist
     protected void onCreate() {
         this.created_At = new Date();
@@ -34,5 +102,5 @@ public class Project {
         this.updated_At = new Date();
     }
 
-       
+
 }
