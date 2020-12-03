c2822b8b0a8f5f1930ef4aea858187845e91f661
kmat1912 <kmry>
2020-12-02 21:54:38 -0500

    Project Object Validation 1

--- PPMToolFullStack/ppmtool/src/main/java/io/agileintelligence/ppmtool/domain/Project.java
+++ PPMToolFullStack/ppmtool/src/main/java/io/agileintelligence/ppmtool/domain/Project.java
@@ -2,12 +2,18 @@ package io.agileintelligence.ppmtool.domain;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 
 @Entity
 public class Project {
@@ -15,14 +21,33 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
+    @NotBlank(message = "Project name is required")
     private String projectName;
+    
+    @NotBlank(message = "Project Identifier is required")
+    @Size(min = 4, max = 5, message = "Please use 4 to 5 characters")
+    @Column(updatable = false, unique = true)
     private String projectIdentifier;
+    
+    @NotBlank(message = "Project Description is required")
     private String description;
+    
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date start_date;
+    
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date end_date;
-
+    
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date created_At;
+    
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updated_At;
+
+    //No argument constructor 
+    public Project() {
+    }
 
     public Long getId() {
         return this.id;
@@ -86,10 +111,6 @@ public class Project {
 
     public void setUpdated_At(Date updated_At) {
         this.updated_At = updated_At;
-    }
-
-    //No argument constructor 
-    public Project() {
     }
 
     @PrePersist
