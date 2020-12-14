cd3c523b68df6fadfc17ec757e5e17a95fa96fc0
kmat1912 <khalilmat1912@gmail.com>
2020-12-14 17:02:09 -0500

    Making the error feedback return only field and description

--- PPMToolFullStack/ppmtool/src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/ppmtool/src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
@@ -1,5 +1,9 @@
 package io.agileintelligence.ppmtool.web;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.validation.BindingResult;
 import  org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
 
 import io.agileintelligence.ppmtool.domain.Project;
 import io.agileintelligence.ppmtool.services.ProjectService;
@@ -27,12 +32,22 @@ public class ProjectController {
     //The ? indicates a generic type
     //@Valid annotation ensures the validation of the whole object
     //BindingResult: Serves as result holder for a DataBinder
+    //getFieldErrors: extraxt error from returned error list response
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
         
         if(result.hasErrors()) {
-            return new ResponseEntity<String>("Invalid Project Object", HttpStatus.BAD_REQUEST);
+
+            Map<String, String> errorMap = new HashMap<>();
+            //Filter to return only the field (variable) and the errormessage out of the JSON response (Must return 3 entries: description, projectName, projectIdentifier)
+            for(FieldError error: result.getFieldErrors()){
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+
+            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
         }
+
+
 
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
