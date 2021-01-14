fe9db00f048d9ff0858464dcd63165e423b0b30d
kmat1912 <khalilmat1912@gmail.com>
2021-01-13 23:19:42 -0500

    functional components and Route/Link

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,24 +1,23 @@
 import React, { Component } from 'react'
+import CreateProjectButton from './project/CreateProjectButton';
 import ProjectItem from './project/ProjectItem';
  class Dashboard extends Component {
     render() {
         return (
             <div className="projects">
-        <div className="container">
-            <div className="row">
-                <div className="col-md-12">
-                    <h1 className="display-4 text-center">Projects</h1>
-                    <br />
-                    <a href="ProjectForm.html" className="btn btn-lg btn-info">
-                        Create a Project
-                    </a>
-                    <br />
-                    <hr />
-                    <ProjectItem />
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <h1 className="display-4 text-center">Projects</h1>
+                            <br />
+                            <CreateProjectButton />
+                            <br />
+                            <hr />
+                            <ProjectItem />
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
         );
     }
 }
