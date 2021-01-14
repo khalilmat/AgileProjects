09f2f1de3aa7073334d11ed6cd306bd078717d10
kmat1912 <khalilmat1912@gmail.com>
2021-01-13 22:13:42 -0500

    adding bootstrap

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React, { Component } from "react";
 import Dashboard from './components/Dashboard';
 import Header from './components/Layout/Header';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 
 class App extends Component {
