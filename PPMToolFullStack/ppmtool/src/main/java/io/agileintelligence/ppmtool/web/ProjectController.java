e1fb39659827b1abbd5ccd510bf19d9491edf75f
kmat1912 <khalilmat1912@gmail.com>
2021-01-19 17:18:03 -0500

    Add CrossOrigin to ProjectController

--- PPMToolFullStack/ppmtool/src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/ppmtool/src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.RequestEntity.BodyBuilder;
 import org.springframework.http.StreamingHttpOutputMessage.Body;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,7 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 import ch.qos.logback.core.status.Status;
 
 import org.springframework.validation.BindingResult;
-import  org.springframework.validation.Errors;
+import org.springframework.validation.Errors;
 import org.springframework.validation.FieldError;
 
 import io.agileintelligence.ppmtool.domain.Project;
@@ -32,21 +33,24 @@ import io.agileintelligence.ppmtool.services.ProjectService;
 
 @RestController
 @RequestMapping("/api/project")
+@CrossOrigin
 public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
     @Autowired
     private MapValidationErrorService mapValidationErrorService;
-    //The ? indicates a generic type
-    //@Valid annotation ensures the validation of the whole object
-    //BindingResult: Serves as result holder for a DataBinder
-    //getFieldErrors: extraxt error from returned error list response
+
+    // The ? indicates a generic type
+    // @Valid annotation ensures the validation of the whole object
+    // BindingResult: Serves as result holder for a DataBinder
+    // getFieldErrors: extraxt error from returned error list response
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
-        
+
         ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
-        if(errorMap!=null) return errorMap;
+        if (errorMap != null)
+            return errorMap;
 
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
@@ -54,24 +58,27 @@ public class ProjectController {
 
     @GetMapping("/{projectId}")
     public ResponseEntity<?> getProjectById(@PathVariable String projectId) {
-        
+
         Project project = projectService.findProjectByIdentifier(projectId);
         return new ResponseEntity<Project>(project, HttpStatus.OK);
     }
 
-    /*Mapping for return all projectsm retruns project service
-      layer findAllProjects*/
+    /*
+     * Mapping for return all projectsm retruns project service layer
+     * findAllProjects
+     */
     @GetMapping("/all")
-    public Iterable<Project> getAllProjects(){
+    public Iterable<Project> getAllProjects() {
         return projectService.findAllProjects();
     }
 
-    /*Mapping for delete project*/
-    @DeleteMapping("/{projectId}") 
-        public ResponseEntity<?> deleteProject(@PathVariable String projectId) {
-            projectService.deleteProjectByIdentifier(projectId);
+    /* Mapping for delete project */
+    @DeleteMapping("/{projectId}")
+    public ResponseEntity<?> deleteProject(@PathVariable String projectId) {
+        projectService.deleteProjectByIdentifier(projectId);
 
-            return new ResponseEntity<String>("Project with ID: '"+projectId+"' was deleted successfuly", HttpStatus.OK);
+        return new ResponseEntity<String>("Project with ID: '" + projectId + "' was deleted successfuly",
+                HttpStatus.OK);
 
-        }
+    }
 }
